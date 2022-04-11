71fd21cf28199865035d640376bc0f87aec1e7b2
Pooh5159 <Pierson.Pauline@gmail.com>
2022-04-11 10:46:09 +0200

    exercise done

--- M11001-2.php
+++ M11001-2.php
@@ -1,6 +1,19 @@
 <?php
 
 // Créer une constante avec la fonction adaptée, et lui donner le nom et la valeur 18
+define("number", "18");
+var_dump(number);
 
+// Utiliser cette constante dans une nouvelle variable de type string, ayant pour contenu "Le numéro des pompiers est le " puis concaténer votre constante
+$phrase = "Le numéro des pompiers est le ".number;
+echo $phrase;
+echo "<br>";
 
-// Utiliser cette constante dans une nouvelle variable de type string, ayant pour contenu "Le numéro des pompiers et le " puis concaténer votre constante
+// Concaténer les chaines de caractères suivantes :
+//  - La référence "19805" est 
+//  - l'appareil le plus vendu dans
+//  - l'agglomération de "Pont-l'évêque"
+$part1 = "La référence \"19805\" est";
+$part2 = "l'appareil le plus vendu dans";
+$part3 = "l'agglomération de \"Pont-l'évêque\"";
+echo "$part1 $part2 $part3";
